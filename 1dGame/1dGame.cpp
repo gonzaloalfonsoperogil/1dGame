55a9a98e7da034eb6cf82a9e70b732d89901bf4d
pointfree22 <gonzaloapp@gmail.com>
2017-11-14 21:50:42 +0100

    New struct

--- 1dGame/1dGame.cpp
+++ 1dGame/1dGame.cpp
@@ -13,156 +13,182 @@
 #define KEY_L 108
 #define KEY_K 107
 
+void UpdateDraw();
+void UpdateBullet();
+void UpdateEnemy();
+void UpdateMushroom();
+void Input();
+
+bool		end				= false;
+int			screenWith		= 90;
+const int	initPosPlayer	= 20;
+int			posPlayer		= initPosPlayer;
+int			posBullet		= -1;
+int			posMushroom		= -1;
+int			countMushroom	= 0;
+int			randomMushroom;
+int			posEnemy		= -1;
+int			countEnemy		= 0;
+int			randomEnemy;
+int			sideEnemy;
+int			score			= 0;
+int			lives			= 3;
+int			EnemiesX[4]		= {};
+
 int main()
 {
-	bool end = false;
-	int  screenWith = 50;
-	const int  initPosPlayer = 20;
-	int  posPlayer = initPosPlayer;
-	int  posBullet = -1;
-	int  posMushroom = -1;
-	int  countMushroom = 0;
-	int  randomMushroom;
-	int  posEnemy = -1;
-	int  countEnemy = 0;
-	int  randomEnemy;
-	int  sideEnemy;
-	int  score = 0;
-	int  lives = 3;
-
 	printf("\n\n\n\n\n\n\n\n");
 	while (!end) {
 
 		// Draw loop
-		for (int i = 0; i <= screenWith; i++) {
-			if (i == posPlayer) {
-				printf("8");
-			}
-			else if (i == posBullet && posPlayer > posBullet) {
-				printf("<");
-			}
-			else if (i == posBullet && posPlayer < posBullet) {
-				printf(">");
-			}
-			else if (i == posEnemy) {
-				printf("@");
-			}
-			else if (i == posMushroom) {
-				printf(":");
-			}
-			else {
-				printf("_");
-			}
-
-		}
-		printf("\tScore:%i\t Lives:%i", score, lives);
-		printf("\r");
+		UpdateDraw();
 
 		//Bullet logic
-		if (posBullet < posPlayer || posBullet == posPlayer) {
-			posBullet--;
+		UpdateBullet();
+
+		//Enemy logic
+		UpdateEnemy();
+
+		//Mushroom logic
+		UpdateMushroom();
+
+		//INPUT
+		Input();
+
+		Sleep(50);
+	}
+
+	return 0;
+}
+
+void UpdateDraw() {
+	for (int i = 0; i <= screenWith; i++) {
+		if (i == posPlayer) {
+			printf("8");
 		}
-		else if (posBullet > posPlayer) {
-			posBullet++;
+		else if (i == posBullet && posPlayer > posBullet) {
+			printf("<");
+		}
+		else if (i == posBullet && posPlayer < posBullet) {
+			printf(">");
+		}
+		else if (i == posEnemy) {
+			printf("@");
+		}
+		else if (i == posMushroom) {
+			printf(":");
+		}
+		else {
+			printf("_");
+		}
+	}
+
+	printf("\tScore:%i\t Lives:%i", score, lives);
+	printf("\r");
+}
+
+void UpdateBullet() {
+	if (posBullet < posPlayer || posBullet == posPlayer) {
+		posBullet--;
+	}
+	else if (posBullet > posPlayer) {
+		posBullet++;
+	}
+	if (posEnemy == posBullet) {
+		posEnemy = -1;
+		posBullet = -1;
+	}
+}
+
+void UpdateEnemy() {
+	if (posEnemy<0 || posEnemy>screenWith) {
+		randomEnemy = rand() % 21;
+		countEnemy = 0;
+		sideEnemy = rand() % 2;
+	}
+	if (countEnemy == randomEnemy) {
+		if (sideEnemy == 0) {
+			posEnemy = 0;
+		}
+		else {
+			posEnemy = screenWith;
+		}
+	}
+	else if (countEnemy >= randomEnemy) {
+		if (sideEnemy == 0) {
+			posEnemy++;
+		}
+		else {
+			posEnemy--;
 		}
 		if (posEnemy == posBullet) {
 			posEnemy = -1;
 			posBullet = -1;
 		}
-
-
-		//Enemy logic
-		if (posEnemy<0 || posEnemy>screenWith) {
-			randomEnemy = rand() % 21;
-			countEnemy = 0;
-			sideEnemy = rand() % 2;
-		}
-		if (countEnemy == randomEnemy) {
-			if (sideEnemy == 0) {
-				posEnemy = 0;
-			}
-			else {
-				posEnemy = screenWith;
-			}
-		}
-		else if (countEnemy >= randomEnemy) {
-			if (sideEnemy == 0) {
-				posEnemy++;
-			}
-			else {
-				posEnemy--;
-			}
-			if (posEnemy == posBullet) {
-				posEnemy = -1;
-				posBullet = -1;
-			}
-			if (posEnemy == posPlayer) {
-				posPlayer = initPosPlayer;
-				lives--;
-				if (lives < 0) {
-					end = true;
-				}
-			}
-			
-		}
-		countEnemy++;
-
-		//Mushroom logic
-		if (posMushroom < 0 || posMushroom>screenWith) {
-			randomMushroom = rand() % 21;
-			countMushroom = 0;
-		}
-		if (countMushroom == randomMushroom) {
-			posMushroom = rand() % screenWith + 1;
-		}
-
-		else if (countMushroom > randomMushroom) {
-			if (countMushroom >= randomMushroom + 80) {
-				posMushroom = -1;
-			}
-			else if (posMushroom == posPlayer) {
-				posMushroom = -1;
-				posBullet = -1;
-				score += 10;
-			}
-		}
-		countMushroom++;
-
-		//INPUT
-		if (_kbhit()) {
-			int key = _getch();
-			switch (key) {
-			case ESC:
+		if (posEnemy == posPlayer) {
+			posPlayer = initPosPlayer;
+			lives--;
+			if (lives < 0) {
 				end = true;
-				break;
-
-			case KEY_A:
-				if (posPlayer > 0) {
-					posPlayer--;
-				}
-				break;
-
-			case KEY_D:
-				if (posPlayer < screenWith) {
-					posPlayer++;
-				}
-				break;
-
-			case KEY_K:
-				if (posBullet<0 || posBullet > screenWith) {
-					posBullet = posPlayer - 1;
-				}
-				break;
-
-			case KEY_L:
-				if (posBullet<0 || posBullet > screenWith) {
-					posBullet = posPlayer + 1;
-				}
-				break;
 			}
 		}
-		Sleep(50);
+	}
+	countEnemy++;
+}
+
+void UpdateMushroom() {
+	if (posMushroom < 0 || posMushroom>screenWith) {
+		randomMushroom = rand() % 21;
+		countMushroom = 0;
+	}
+	if (countMushroom == randomMushroom) {
+		posMushroom = rand() % screenWith + 1;
 	}
 
-	return 0;
+	else if (countMushroom > randomMushroom) {
+		if (countMushroom >= randomMushroom + 80) {
+			posMushroom = -1;
+		}
+		else if (posMushroom == posPlayer) {
+			posMushroom = -1;
+			posBullet = -1;
+			score += 10;
+		}
+	}
+	countMushroom++;
+}
+
+void Input() {
+	if (_kbhit()) {
+		int key = _getch();
+		switch (key) {
+		case ESC:
+			end = true;
+			break;
+
+		case KEY_A:
+			if (posPlayer > 0) {
+				posPlayer--;
+			}
+			break;
+
+		case KEY_D:
+			if (posPlayer < screenWith) {
+				posPlayer++;
+			}
+			break;
+
+		case KEY_K:
+			if (posBullet<0 || posBullet > screenWith) {
+				posBullet = posPlayer - 1;
+			}
+			break;
+
+		case KEY_L:
+			if (posBullet<0 || posBullet > screenWith) {
+				posBullet = posPlayer + 1;
+			}
+			break;
+		}
+	}
 }
